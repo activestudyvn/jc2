b09ee79b718e42a66f37890d3ccfd4d9ea413a9d
dangxunb <dang.gffg@gmail.com>
2015-11-12 13:06:20 +0700

    Tested, added comment, optimized code

--- WEEK_3/BaiTap/DangNH/src/Ex2.java
+++ WEEK_3/BaiTap/DangNH/src/Ex2.java
@@ -3,8 +3,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 /**
@@ -17,58 +17,75 @@ public class Ex2 {
     static final File input = new File("input");
     static final File output = new File("output");
     static final DateTimeFormatter defaultDateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-    static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    //list dir in input and output folder
+    static ArrayList<File> listInputDir = new ArrayList<>();
+    static ArrayList<File> listOutputDir = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        ArrayList<File> listInputDir = new ArrayList<>();
-        ArrayList<File> listOutputDir = new ArrayList<>();
-        File[] filesInput;
-        File[] filesOutput;
+        //Get list dir in input and output folder
         for (File inputSubDir : input.listFiles()) {
             listInputDir.add(inputSubDir);
         }
         for (File outputSubDir : output.listFiles()) {
             listOutputDir.add(outputSubDir);
         }
+
         for (File inputDir : listInputDir) {
-            filesInput = inputDir.listFiles(new FileFilter() {
+            //get all files in inputDir. File name must match inputPattern
+            File[] filesInput = inputDir.listFiles(new FileFilter() {
                 @Override
                 public boolean accept(File file) {
                     return inputPattern.matcher(file.getName()).matches();
                 }
             });
-            Optional<File> outputDir = listOutputDir.parallelStream().filter(file -> file.getName().equals(inputDir.getName())).findAny();
+
+            //get outputDir in output folder which have the same name with inputDir folder
+            Optional<File> outputDir = listOutputDir.parallelStream()
+                    .filter(file -> file.getName().equals(inputDir.getName())).findAny();
             if (!outputDir.isPresent()) continue;
-            filesOutput = outputDir.get().listFiles(new FileFilter() {
+
+            //get all files in inputDir. File name must match outputPattern
+            File[] filesOutput = outputDir.get().listFiles(new FileFilter() {
                 @Override
                 public boolean accept(File file) {
                     return outputPattern.matcher(file.getName()).matches();
                 }
             });
-            HashSet<String> inputSet = getPhoneSet(filesInput);
-            HashSet<String> outputSet = getPhoneSet(filesOutput);
-            HashSet<String> inputTemp = (HashSet<String>) inputSet.clone();
-            inputSet.removeAll(outputSet);
+
+            //get a set of phone string in input and output folder
+            Set<String> inputSet = getPhoneSet(filesInput);
+            Set<String> outputSet = getPhoneSet(filesOutput);
+            Set<String> tempInputSet = new HashSet<>(inputSet);
             LocalDate fileDate = LocalDate.parse(inputDir.getName(), defaultDateFormatter);
+            inputSet.removeAll(outputSet);
             System.out.println(fileDate.format(dateFormatter) + " Input co nhung Output ko co: " + inputSet);
-            outputSet.removeAll(inputTemp);
-            System.out.println("Output co nhung Input ko co: " + outputSet);
-        }
-        for (File outputDir : listOutputDir) {
-            filesOutput = outputDir.listFiles();
+            outputSet.removeAll(tempInputSet);
+            System.out.println(fileDate.format(dateFormatter) + "Output co nhung Input ko co: " + outputSet);
         }
     }
 
-    static HashSet<String> getPhoneSet(File[] fileArr) throws IOException {
-        HashSet<String> phoneSet = new HashSet<>();
+    /**
+     * Get a set of phone number from a file array
+     *
+     * @param fileArr file array to get phone number
+     * @return Set\<String\>
+     * @throws IOException when I/O error occur
+     */
+    static Set<String> getPhoneSet(File[] fileArr) throws IOException {
+        Set<String> phoneSet = new HashSet<>();
         for (File file : fileArr) {
             try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                 String s = "";
                 int count = 1;
                 while ((s = reader.readLine()) != null) {
-                    if (phonePattern.matcher(s).matches()) phoneSet.add(s);
+                    if (phonePattern.matcher(s).matches()) {
+                        phoneSet.add(s);
+                    }
                 }
             }
         }
         return phoneSet;
     }
+
 }
